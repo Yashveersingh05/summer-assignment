a798aef3cd8174c15e7f1428810bacaacaf9c5c2
Yashveersingh05 <yashunknown4@gmail.com>
2026-06-15 12:35:10 +0530

    day10

---
+++ question4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++) {
+
+        for (char ch = 'A'; ch < 'A' + i; ch++) {
+            cout << ch;
+        }
+
+        for (char ch = 'A' + i - 2; ch >= 'A'; ch--) {
+            cout << ch;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
