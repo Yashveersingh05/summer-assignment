d3e98d57dc62126d0be2300f3c2585fdec0baf13
Yashveersingh05 <yashunknown4@gmail.com>
2026-06-15 12:53:40 +0530

    day14

---
+++ day14/question2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key, count = 0;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element: ";
+    cin >> key;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key)
+            count++;
+    }
+
+    cout << "Frequency = " << count;
+
+    return 0;
+}
